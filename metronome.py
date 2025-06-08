23143c10ecb6b29208a8e128a72970adfe372b52
cooperg456 <cooperg456@gmail.com>
2025-06-07 21:10:46 -0400

    switched from playsound3 to sounddevice

--- metronome.py
+++ metronome.py
@@ -1,14 +1,23 @@
-from playsound3 import playsound
+import sounddevice as sd
+import numpy as np
 import time
 
 bpm = 120
 interval = 60 / bpm
 
+frequency = 440
+duration = 0.05
+fs = 48000
+
+t = np.linspace(0, duration, int(fs * duration), endpoint=False)
+
+sine_wave = 0.5 * np.sin(2 * np.pi * frequency * t)
+
 def main():
     STARTUP_TIME = time.perf_counter()
     
     while True:
-        playsound("808cb.wav", block=False)
+        sd.play(sine_wave, fs)
 
         # hybrid waiting to quantize each beat
         sleep_time = interval - (time.perf_counter() - STARTUP_TIME) % interval - 0.005
