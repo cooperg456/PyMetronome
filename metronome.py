a4d7fc43a779a93460cd2b4536ba598c45d6d2e9
cooperg456 <cooperg456@gmail.com>
2025-06-08 15:10:17 -0400

    lower level control for beter functionality

--- metronome.py
+++ metronome.py
@@ -1,23 +1,53 @@
-import sounddevice as sd
-import numpy as np
+import threading
 import time
 
-bpm = 120
+import sounddevice as sd
+import numpy as np
+
+
+bpm = 400
 interval = 60 / bpm
 
 frequency = 440
 duration = 0.05
 fs = 48000
 
-t = np.linspace(0, duration, int(fs * duration), endpoint=False)
-
-sine_wave = 0.5 * np.sin(2 * np.pi * frequency * t)
-
 def main():
+    start_idx = 0
+    frequency = 440
+    amplitude = 0
+    amp_lock = threading.Lock()
+
+    output_device_info = sd.query_devices(sd.default.device[1], 'output')
+    #print(output_device_info)
+    samplerate = output_device_info['default_samplerate']
+
+    def callback(outdata, frames, time_data, status):
+        if status:
+            print(status)
+        with amp_lock:
+            volume = amplitude
+        nonlocal start_idx
+        t = (start_idx + np.arange(frames)) / samplerate
+        t = t.reshape(-1, 1)
+        outdata[:] = volume * np.sin(2 * np.pi * frequency * t)
+        start_idx += frames
+
+    O_STREAM = sd.OutputStream(device=sd.default.device[1],
+                               channels=1,
+                               dtype='float32',
+                               latency='low',
+                               callback=callback,
+                               samplerate=samplerate,)
+
+    O_STREAM.start()
     STARTUP_TIME = time.perf_counter()
-    
     while True:
-        sd.play(sine_wave, fs)
+        with amp_lock:
+            amplitude = 1
+        time.sleep(0.05)
+        with amp_lock:
+            amplitude = 0
 
         # hybrid waiting to quantize each beat
         sleep_time = interval - (time.perf_counter() - STARTUP_TIME) % interval - 0.005
@@ -25,6 +55,7 @@ def main():
             time.sleep(sleep_time)
         while (time.perf_counter() - STARTUP_TIME) % interval > 0.0001:
             pass
+
 
 if __name__ == "__main__":
     main()
